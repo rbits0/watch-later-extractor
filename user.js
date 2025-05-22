158ce94800efefa984bff9c1c94a3efc76621ff5
rbits0 <rbits0@gmail.com>
2025-05-22 12:52:15 +1000

    Fix div sometimes not showing

--- user.js
+++ user.js
@@ -69,10 +69,13 @@ function runScript() {
         width: 100vw;
         height: 100vh;
         position: fixed;
+        top: 0;
+        left: 0;
         display: flex;
         align-items: center;
         justify-content: center;
         z-index: 9999;
+        padding: 1rem;
     `
     flex.appendChild(box);
 
